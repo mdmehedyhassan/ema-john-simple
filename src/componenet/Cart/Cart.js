e71a4f6bc6473c4a3cb3a235cfdf89d2e69758e2
mdmehedyhassan <mdmehedyh55@gmail.com>
2021-03-06 13:00:06 +0600

    last finish

--- src/componenet/Cart/Cart.js
+++ src/componenet/Cart/Cart.js
@@ -32,11 +32,11 @@ const Cart = (props) => {
     return (
         <div>
             <h4>Order Summary </h4>
-            <p>Item Ordered: {cart.length} </p>
-            <p>Product Price: {formatNumber(total)}</p>
-            <p><small>Shipping Cost: {shipping}</small></p>
-            <p><small>Tax: {tax}</small></p>
-            <p>Total Price {grandTotal}</p>
+            <p>Item Ordered: ${cart.length} </p>
+            <p>Product Price: ${formatNumber(total)}</p>
+            <p><small>Shipping Cost: ${shipping}</small></p>
+            <p><small>Tax: ${tax}</small></p>
+            <p>Total Price ${grandTotal}</p>
             <br/>
             {
                 props.children
